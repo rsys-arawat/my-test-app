d60ab6848a1c705952653b1d16d8cf6ac5884108
rsys-arawat <108254057+rsys-arawat@users.noreply.github.com>
2022-07-02 00:11:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 });
 
 function fetchUserInputEML(req, res, next) {
-  const myUrl = publicIp + "/gatherAction";
+  const myUrl = publicIp + "gatherAction";
 
   // If you are using NGROK - edit the path - see below NGROK section for details.
 
